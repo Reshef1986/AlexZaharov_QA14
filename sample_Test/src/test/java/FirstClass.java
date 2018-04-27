d060d32b9610e70144a9cdccc1c7d1e7f1197a5a
Reshef1986 <reshef1986@gmail.com>
2018-04-27 12:04:05 +0300

    created first test

---
+++ sample_Test/src/test/java/FirstClass.java
@@ -0,0 +1,57 @@
+
+
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class FirstClass {
+  private WebDriver driver;
+
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void searchWebDriverInChrome() throws Exception {
+    driver.get("https://www.google.co.il/webhp?authuser=1");
+    driver.findElement(By.id("lga")).click();
+    driver.findElement(By.id("lst-ib")).click();
+    driver.findElement(By.id("lst-ib")).clear();
+    driver.findElement(By.id("lst-ib")).sendKeys("wabDriver");
+    driver.findElement(By.id("lst-ib")).sendKeys(Keys.ENTER);
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
